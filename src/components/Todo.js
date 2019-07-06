4555495c08767476024f3a95fa5fef51bb8743a6
Vincent Pace <vpace@nhpace.net>
2019-07-06 16:11:46 -0500

    Add useState hook to project

---
+++ src/components/Todo.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const Todo = props => {
+    const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
+
+    const inputChangeHandler = event => {
+        setTodoName(event.target.value);
+    };
+
+    const todoAddHandler = () => {
+        setTodoList(todoList.concat(todoName));
+    };
+
+    return <React.Fragment>
+        <input
+            type="text"
+            placeholder="Todo"
+            onChange={inputChangeHandler}
+            value={todoName}
+        />
+        <button type="button" onClick={todoAddHandler}>
+            Add
+        </button>
+        <ul>
+            {todoList.map(
+                todo => <li key={todo}>{todo}</li>)}
+        </ul>
+    </React.Fragment>;
+};
+
+export default Todo;
