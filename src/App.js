cbd2599bdf5c4459fdb30c9e5feed06d04d1dad4
Vincent Pace <vpace@nhpace.net>
2019-07-06 20:30:27 -0500

    Implement useEffect hook in project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todo from './components/Todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
-function App() {
-  return (
-    <div className="App">
-      <Todo />
-    </div>
-  );
-}
+const App = props => {
+    const [page, setPage] = useState('auth');
+
+
+    return (
+        <div className="App">
+            <Header/>
+            <hr/>
+            <Todo/>
+            <Auth/>
+        </div>
+    );
+};
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Todo = props => {
     const [todoName, setTodoName] = useState('');
     const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://react-first-hooks.firebaseio.com/react-first-hooks.json').then(response => {
+            console.log(response);
+            const todoData = response.data;
+            const todos = [];
+
+            for (const key in todoData) {
+                todos.push({id: key, name: todoData[key].name})
+            }
+
+            setTodoList(todos);
+        });
+        return () => {
+            console.log('Cleanup')
+        };
+    }, [todoName]);
+
+    const mouseMoveHandler = event => {
+        console.log(event.clientX, event.clientY);
+    };
+
+
+    useEffect(() => {
+        document.addEventListener('mousemove', mouseMoveHandler);
+        return () => {
+            document.removeEventListener('mousemove', mouseMoveHandler);
+        };
+    });
+
 
     const inputChangeHandler = event => {
         setTodoName(event.target.value);
@@ -10,6 +41,10 @@ const Todo = props => {
 
     const todoAddHandler = () => {
         setTodoList(todoList.concat(todoName));
+        axios.post('https://react-first-hooks.firebaseio.com/react-first-hooks.json', {name: todoName})
+            .then(response => {
+                console.log(response);
+            });
     };
 
     return <React.Fragment>
@@ -24,7 +59,7 @@ const Todo = props => {
         </button>
         <ul>
             {todoList.map(
-                todo => <li key={todo}>{todo}</li>)}
+                todo => <li key={todo.id}>{todo.name}</li>)}
         </ul>
     </React.Fragment>;
 };
